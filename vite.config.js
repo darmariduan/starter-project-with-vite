6803f7678a17a642b0e6443d4d5d4788da023436
Darma Riduan <dharmarhiduwhan@gmail.com>
2025-12-18 10:55:44 +0700

    fix: add base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Base path untuk GitHub Pages
+  base: '/starter-project-with-vite/',
+  
   root: resolve(__dirname, 'src'),
   publicDir: resolve(__dirname, 'src', 'public'),
   build: {
